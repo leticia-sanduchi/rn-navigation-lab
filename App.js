a868ff636f8f17045d14beb23ed7def8ae7ebf89
leticia-sanduchi <leticia.sanduchi@aluno.senai.br>
2026-05-31 00:13:41 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import AppNavigator from "./src/navigation";
 export default function App() {
   return (
     <SafeAreaProvider>
-      {/* Chamar o NavigationContainer */}
-      {/* Chamar o Componente do navigation/index.js */}
+      <NavigationContainer>
+        <AppNavigator />
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
