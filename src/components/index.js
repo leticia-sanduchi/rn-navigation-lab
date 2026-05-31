3fffed80eb2ee92db0637a8fa608b961ca471a54
leticia-sanduchi <leticia.sanduchi@aluno.senai.br>
2026-05-31 00:15:12 -0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,2 @@
-// Exportacoes centralizadas dos componentes (barrel export)
-// Importar de um unico lugar: import { BotaoAcao } from '../components';
-export { default as BotaoAcao } from './BotaoAcao';
-
-// TODO: apos criar CardJogo.js, adicione a linha abaixo:
-// export { default as CardJogo } from './CardJogo';
+export { default as BotaoAcao } from "./BotaoAcao";
+export { default as CardJogo } from "./CardJogo";
